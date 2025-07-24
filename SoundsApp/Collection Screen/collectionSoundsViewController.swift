0b51d5b5772434648398535cd75638392832afbe
Humayun Tariq <humayun0712@gmail.com>
2025-07-24 14:09:10 +0500

    made entities and their attributes and relationship for storing sounds and collections on device.

---
+++ SoundsApp/Collection Screen/collectionSoundsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  collectionSoundsViewController.swift
+//  SoundsApp
+//
+//  Created by Humayun Tariq on 24/07/2025.
+//
+
+import UIKit
---
+++ SoundsApp/coreDataController.swift
@@ -0,0 +1,14 @@
+//
+//  coreDataController.swift
+//  SoundsApp
+//
+//  Created by Humayun Tariq on 24/07/2025.
+//
+import CoreData
+import UIKit
+
+class CoreDataController {
+    
+    
+}
+
